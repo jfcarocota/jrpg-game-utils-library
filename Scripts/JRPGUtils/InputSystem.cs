5d4a14786e9dc8c0725e0abfb05a92b6ba7fee2a
jfcarocota <jfcarocota@gmail.com>
2019-12-02 07:31:45 -0800

    publish library

---
+++ Scripts/JRPGUtils/InputSystem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace JRPGUtils.InputSystem
+{
+    public static class InputSystem
+    {
+        /// <summary>
+        /// Returns the direction of the keyboard or gamepad stick.
+        /// </summary>
+        public static Vector2 Axis{ get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); }
+
+        /// <summary>
+        /// Returns the direction of the keyboard or gamepad stick in 1 units.
+        /// </summary>
+        public static Vector2 AxisNormalized { get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized; }
+
+        /// <summary>
+        /// Returns the direction of the keyboard or gamepad stick multiplied by delta time.
+        /// </summary>
+        public static Vector2 AxisDelta { get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime; }
+
+        /// <summary>
+        /// Returns the direction of the keyboard or gamepad stick in 1 units multiplied by delta time.
+        /// </summary>
+        public static Vector2 AxisDeltaNormalized { get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * Time.deltaTime; }
+    }
+}
---
+++ Scripts/JRPGUtils/MovementSystem.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+
+namespace JRPGUtils.MovementSystem
+{
+    public static class MovementSystem
+    {
+
+        /// <summary>
+        /// Move a sprite in XY direction using the axis of keyboard or gamepad.
+        /// </summary>
+        /// <param name="t">Transform component of the sprite.</param>
+        /// <param name="moveSpeed">Speed of the movement.</param>
+        /// <param name="axis">Direction of the movement based on keyboard or gamepad axis.</param>
+        public static void Move2DTopdown(Transform t, float moveSpeed, Vector2 axis)
+        {
+            t.Translate(axis * moveSpeed * axis);
+        }
+    }
+}
